8623b3820dc1958f3a589cf5b844a1f3a4321589
jgolden5 <jonathan.golden.gurr@gmail.com>
2026-04-12 09:13:56 -0700

    .

--- transfer_tmux_config.sh
+++ transfer_tmux_config.sh
@@ -8,18 +8,10 @@ main() {
     return 1
   fi
 
-  tmux source ~/.tmux.conf
-  if [[ "$?" == "0" ]]; then 
-    echo "(2) ✅ tmux config file was sourced successfully"
-  else
-    echo "(2) ❌ tmux config file was NOT sourced successfully. Quitting now"
-    return 1
-  fi
-  
-  read -n1 -p "(3) ✅ tmux is ready to start! Start session? " confirmation
+  read -n1 -p "(2) ✅ tmux is ready to start! Start session? " confirmation
   echo
   if [[ $confirmation == 'y' ]]; then
-    echo "(4) Starting tmux session now..." && tmux
+    echo "(3) Starting tmux session now..." && tmux
   fi
 }
 
