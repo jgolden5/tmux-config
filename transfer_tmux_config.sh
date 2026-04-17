86ea9477a277df1f4faf8535201e611afc017285
jgolden5 <jonathan.golden.gurr@gmail.com>
2026-04-17 14:04:25 -0700

    implemented session naming and re-implemented transfer_tmux_config.sh to source tmux file

--- transfer_tmux_config.sh
+++ transfer_tmux_config.sh
@@ -7,8 +7,12 @@ main() {
     echo "(1) ❌ tmux config did not copy successfully. Quitting now"
     return 1
   fi
-
-  read -n1 -p "(2) ✅ tmux is ready to start! Start session? " confirmation
+  tmux source ~/.tmux.conf
+  if [[ "$?" == 0 ]]; then
+    read -n1 -p "(2) ✅ tmux sourced successfully and is ready to start! Start session? " confirmation
+  else
+    echo "(2) ❌ tmux did not source successfully. Quitting now"
+  fi
   echo
   if [[ $confirmation == 'y' ]]; then
     echo "(3) Starting tmux session now..." && tmux
